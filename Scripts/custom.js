de48098bec1ca2f55f6b0bd62fbd7812e9543e22
Ken <ktkawaguchi@cpp.edu>
2020-02-26 18:13:06 -0800

    Removed stacking and now content toggles with button press

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -1,57 +1,73 @@
 
-// JavaScript Equivalent
-// let btnElectricity = document.getElementById('btn_electricity');
-
-// btnElectricity.addEventListener('click', function(){
-//     let containElectricity = document.getElementById('container_electricity');
-//     if (containElectricity.style.display === "none") {
-//         containElectricity.style.display = "block"
-//     } else {
-//         containElectricity.style.display = "none";
-//     }
-
-// })
-
-// Quick JQuery
-// $('#container_transportation').insertAfter('#startDiv');
-// $('#container_transportation').toggle();
-
+//Button logic to hide and show content-containers
 $('#btn_welcome').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+    
+    $('.content-container').hide();
     $('#container_welcome').insertAfter('#startDiv');
-    $('#container_welcome').toggle();
+    $('#container_welcome').show();
 })
 
 $('#btn_electricity').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+    
+    $('.content-container').hide();
     $('#container_electricity').insertAfter('#startDiv');
-    $('#container_electricity').toggle();
+    $('#container_electricity').show();
 })
 
 $('#btn_record-data').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+
+    $('.content-container').hide();
     $('#container_record-data').insertAfter('#startDiv');
-    $('#container_record-data').toggle();
+    $('#container_record-data').show();
 })
 
 $('#btn_summary').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+
+    $('.content-container').hide();
     $('#container_summary').insertAfter('#startDiv');
-    $('#container_summary').toggle();
+    $('#container_summary').show();
 })
 
 $('#btn_methodology').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+
+    $('.content-container').hide();
     $('#container_methodology').insertAfter('#startDiv');
-    $('#container_methodology').toggle();
+    $('#container_methodology').show();
 })
 
 $('#btn_transportation').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+
+    $('.content-container').hide();
     $('#container_transportation').insertAfter('#startDiv');
-    $('#container_transportation').toggle();
+    $('#container_transportation').show();
 })
 $('#btn_bottles').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+
+    $('.content-container').hide();
     $('#container_bottles').insertAfter('#startDiv');
-    $('#container_bottles').toggle();
+    $('#container_bottles').show();
 })
 $('#btn_cups').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+
+    $('.content-container').hide();
     $('#container_cups').insertAfter('#startDiv');
-    $('#container_cups').toggle();
+    $('#container_cups').show();
 })
 
 // *****Classroom Lighting Content Calculations*****
@@ -65,11 +81,11 @@ $('#btn_add_row').click(function () {
 })
 
 function createRowHTML(counter) {
-    var stringHTML = "<tr><td scope='row'>"+counter+"</td>";
-    stringHTML += "<td><input type='text' id='number_switches"+counter+"'></td>";
-    stringHTML += "<td><input type='text' id='watts_bulb"+counter+"'></td>";
-    stringHTML += "<td><input type='text' id='before_option"+counter+"'></td>";
-    stringHTML += "<td><input type='text' id='after_option"+counter+"'></td></tr>";
+    var stringHTML = "<tr><td scope='row'>" + counter + "</td>";
+    stringHTML += "<td><input type='text' id='number_switches" + counter + "'></td>";
+    stringHTML += "<td><input type='text' id='watts_bulb" + counter + "'></td>";
+    stringHTML += "<td><input type='text' id='before_option" + counter + "'></td>";
+    stringHTML += "<td><input type='text' id='after_option" + counter + "'></td></tr>";
     return stringHTML;
 }
 
@@ -89,33 +105,33 @@ $('#btn_calculate_transportation').click(function () {
     var mpgCarpool = $('#transportation_input9').val();
     var peopleInCarpool = $('#transportation_input11').val();
 
-    gallonsBurnedBeforeAction = gallonsBurned(distanceTraveled,daysDriven,mpg);
+    gallonsBurnedBeforeAction = gallonsBurned(distanceTraveled, daysDriven, mpg).toFixed(2);
     $('#transportation_results1').text(gallonsBurnedBeforeAction);
-    weeklyCarbonEmitted = weeklyCarbonDrivingAlone(gallonsBurnedBeforeAction);
+    weeklyCarbonEmitted = weeklyCarbonDrivingAlone(gallonsBurnedBeforeAction).toFixed(2);
     $('#transportation_results3').text(weeklyCarbonEmitted);
 
     distanceTraveled = $('#transportation_input2').val();
     daysDriven = $('#transportation_input4').val();
     mpg = $('#transportation_input6').val();
 
-    gallonsBurnedAfterAction = gallonsBurned(distanceTraveled,daysDriven,mpg);
+    gallonsBurnedAfterAction = gallonsBurned(distanceTraveled, daysDriven, mpg).toFixed(2);
     $('#transportation_results2').text(gallonsBurnedAfterAction);
-    weeklyCarbonEmitted = weeklyCarbonDrivingAlone(gallonsBurnedAfterAction);
+    weeklyCarbonEmitted = weeklyCarbonDrivingAlone(gallonsBurnedAfterAction).toFixed(2);
     $('#transportation_results4').text(weeklyCarbonEmitted);
 
     distanceTraveled = $('#transportation_input1').val();
-    gallonsBurnedBeforeAction = gallonsBurned(distanceTraveled,daysCarpool,mpgCarpool);
+    gallonsBurnedBeforeAction = gallonsBurned(distanceTraveled, daysCarpool, mpgCarpool).toFixed(2);
     $('#transportation_results5').text(gallonsBurnedBeforeAction);
-    weeklyCarbonEmitted = weeklyCarbonDrivingCarpool(gallonsBurnedBeforeAction, peopleInCarpool);
+    weeklyCarbonEmitted = weeklyCarbonDrivingCarpool(gallonsBurnedBeforeAction, peopleInCarpool).toFixed(2);
     $('#transportation_results7').text(weeklyCarbonEmitted);
 
     distanceTraveled = $('#transportation_input2').val();
     daysCarpool = $('#transportation_input8').val();
     mpgCarpool = $('#transportation_input10').val();
     peopleInCarpool = $('#transportation_input12').val();
-    gallonsBurnedAfterAction = gallonsBurned(distanceTraveled,daysCarpool,mpgCarpool);
+    gallonsBurnedAfterAction = gallonsBurned(distanceTraveled, daysCarpool, mpgCarpool).toFixed(2);
     $('#transportation_results6').text(gallonsBurnedAfterAction);
-    weeklyCarbonEmitted = weeklyCarbonDrivingCarpool(gallonsBurnedAfterAction, peopleInCarpool);
+    weeklyCarbonEmitted = weeklyCarbonDrivingCarpool(gallonsBurnedAfterAction, peopleInCarpool).toFixed(2);
     $('#transportation_results8').text(weeklyCarbonEmitted);
 })
 
