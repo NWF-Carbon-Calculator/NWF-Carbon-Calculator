3426c97281b384737901fcdcd9cb7019128a038e
Ken <ktkawaguchi@cpp.edu>
2020-03-11 12:52:07 -0700

    Fixed nav button issue with it stacking wrong

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -1,66 +1,88 @@
-
-// JavaScript Equivalent
-// let btnElectricity = document.getElementById('btn_electricity');
-
-// btnElectricity.addEventListener('click', function(){
-//     let containElectricity = document.getElementById('container_electricity');
-//     if (containElectricity.style.display === "none") {
-//         containElectricity.style.display = "block"
-//     } else {
-//         containElectricity.style.display = "none";
-//     }
-
-// })
-
-// Quick JQuery
-// $('#container_transportation').insertAfter('#startDiv');
-// $('#container_transportation').toggle();
-
+//Button logic to hide and show content-containers
 $('#btn_welcome').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+    $('.content-container').hide();
     $('#container_welcome').insertAfter('#startDiv');
-    $('#container_welcome').toggle();
+    $('#container_welcome').show();
 })
-
 $('#btn_electricity').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+    $('.content-container').hide();
     $('#container_electricity').insertAfter('#startDiv');
-    $('#container_electricity').toggle();
+    $('#container_electricity').show();
 })
-
+$('#btn_appliance').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+    $('.content-container').hide();
+    $('#container_appliance').insertAfter('#startDiv');
+    $('#container_appliance').show();
+})
+$('#btn_vampire').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+    $('.content-container').hide();
+    $('#container_vampire').insertAfter('#startDiv');
+    $('#container_vampire').show();
+})
 $('#btn_record-data').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+    $('.content-container').hide();
     $('#container_record-data').insertAfter('#startDiv');
-    $('#container_record-data').toggle();
+    $('#container_record-data').show();
 })
-
 $('#btn_summary').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+    $('.content-container').hide();
     $('#container_summary').insertAfter('#startDiv');
-    $('#container_summary').toggle();
+    $('#container_summary').show();
 })
-
 $('#btn_methodology').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+    $('.content-container').hide();
     $('#container_methodology').insertAfter('#startDiv');
-    $('#container_methodology').toggle();
+    $('#container_methodology').show();
 })
-
 $('#btn_transportation').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+    $('.content-container').hide();
     $('#container_transportation').insertAfter('#startDiv');
-    $('#container_transportation').toggle();
+    $('#container_transportation').show();
 })
 $('#btn_bottles').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+    $('.content-container').hide();
     $('#container_bottles').insertAfter('#startDiv');
-    $('#container_bottles').toggle();
+    $('#container_bottles').show();
 })
 $('#btn_cups').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+    $('.content-container').hide();
     $('#container_cups').insertAfter('#startDiv');
-    $('#container_cups').toggle();
+    $('#container_cups').show();
 })
-
 $('#btn_trash').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+    $('.content-container').hide();
     $('#container_trash').insertAfter('#startDiv');
-    $('#container_trash').toggle();
+    $('#container_trash').show();
 })
-
-
-
+$('#btn_paper').click(function () {
+    $(".button-wrapper").find(".btn").removeClass('active');
+    $(this.id).addClass('active');
+    $('.content-container').hide();
+    $('#container_paper').insertAfter('#startDiv');
+    $('#container_paper').show();
+})
 // *****Classroom Lighting Content Calculations*****
 var numberOfRows = 1;
 
@@ -72,11 +94,11 @@ $('#btn_add_row').click(function () {
 })
 
 function createRowHTML(counter) {
-    var stringHTML = "<tr><td scope='row'>"+counter+"</td>";
-    stringHTML += "<td><input type='text' id='number_switches"+counter+"'></td>";
-    stringHTML += "<td><input type='text' id='watts_bulb"+counter+"'></td>";
-    stringHTML += "<td><input type='text' id='before_option"+counter+"'></td>";
-    stringHTML += "<td><input type='text' id='after_option"+counter+"'></td></tr>";
+    var stringHTML = "<tr><td scope='row'>" + counter + "</td>";
+    stringHTML += "<td><input type='text' id='number_switches" + counter + "'></td>";
+    stringHTML += "<td><input type='text' id='watts_bulb" + counter + "'></td>";
+    stringHTML += "<td><input type='text' id='before_option" + counter + "'></td>";
+    stringHTML += "<td><input type='text' id='after_option" + counter + "'></td></tr>";
     return stringHTML;
 }
 
@@ -96,7 +118,7 @@ $('#btn_calculate_transportation').click(function () {
     var mpgCarpool = $('#transportation_input9').val();
     var peopleInCarpool = $('#transportation_input11').val();
 
-    gallonsBurnedBeforeAction = gallonsBurned(distanceTraveled,daysDriven,mpg);
+    gallonsBurnedBeforeAction = gallonsBurned(distanceTraveled, daysDriven, mpg);
     $('#transportation_results1').text(gallonsBurnedBeforeAction);
     weeklyCarbonEmitted = weeklyCarbonDrivingAlone(gallonsBurnedBeforeAction);
     $('#transportation_results3').text(weeklyCarbonEmitted);
@@ -105,13 +127,13 @@ $('#btn_calculate_transportation').click(function () {
     daysDriven = $('#transportation_input4').val();
     mpg = $('#transportation_input6').val();
 
-    gallonsBurnedAfterAction = gallonsBurned(distanceTraveled,daysDriven,mpg);
+    gallonsBurnedAfterAction = gallonsBurned(distanceTraveled, daysDriven, mpg);
     $('#transportation_results2').text(gallonsBurnedAfterAction);
     weeklyCarbonEmitted = weeklyCarbonDrivingAlone(gallonsBurnedAfterAction);
     $('#transportation_results4').text(weeklyCarbonEmitted);
 
     distanceTraveled = $('#transportation_input1').val();
-    gallonsBurnedBeforeAction = gallonsBurned(distanceTraveled,daysCarpool,mpgCarpool);
+    gallonsBurnedBeforeAction = gallonsBurned(distanceTraveled, daysCarpool, mpgCarpool);
     $('#transportation_results5').text(gallonsBurnedBeforeAction);
     weeklyCarbonEmitted = weeklyCarbonDrivingCarpool(gallonsBurnedBeforeAction, peopleInCarpool);
     $('#transportation_results7').text(weeklyCarbonEmitted);
@@ -120,7 +142,7 @@ $('#btn_calculate_transportation').click(function () {
     daysCarpool = $('#transportation_input8').val();
     mpgCarpool = $('#transportation_input10').val();
     peopleInCarpool = $('#transportation_input12').val();
-    gallonsBurnedAfterAction = gallonsBurned(distanceTraveled,daysCarpool,mpgCarpool);
+    gallonsBurnedAfterAction = gallonsBurned(distanceTraveled, daysCarpool, mpgCarpool);
     $('#transportation_results6').text(gallonsBurnedAfterAction);
     weeklyCarbonEmitted = weeklyCarbonDrivingCarpool(gallonsBurnedAfterAction, peopleInCarpool);
     $('#transportation_results8').text(weeklyCarbonEmitted);
@@ -158,23 +180,17 @@ $('#btn_calculate_trash').click(function () {
     calculate();
 })
 
-
 // Calculate carbon impact from user inputs (trash) 
-calculate = function() {
+calculate = function () {
     var trashinput1 = document.getElementById('trash_input1').value;
     var trashinput2 = document.getElementById('trash_input2').value;
-    var answer = parseInt(trashinput2)-parseInt(trashinput1);
+    var answer = parseInt(trashinput2) - parseInt(trashinput1);
     document.getElementById('trash_results1').innerHTML = answer;
 
     var trashinput3 = document.getElementById('trash_input3').value;
     var trashinput4 = document.getElementById('trash_input4').value;
 
 }
-
-
-
-
-
 
 // Clear input values for trash contents
 $('#btn_reset_trash').click(function () {
