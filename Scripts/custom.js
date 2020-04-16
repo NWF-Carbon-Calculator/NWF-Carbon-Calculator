4decfe73a41ea3dd3fb10684e0f6a556ab50b69d
Ken <ktkawaguchi@cpp.edu>
2020-04-15 21:03:17 -0700

    Commented out second function1

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -1050,84 +1050,84 @@ $('#btn_calculate_bottles').click(function () {
   reCalculateSummary();
 })
 
-var bottleCarbonBefore = 0;
-var bottleCarbonAfter = 0;
+// var bottleCarbonBefore = 0;
+// var bottleCarbonAfter = 0;
 
-calculate1 = function () {
-  var bottleinput1 = document.getElementById('bottles_input1').value;
-  var bottleinput2 = document.getElementById('bottles_input2').value;
+// calculate1 = function () {
+//   var bottleinput1 = document.getElementById('bottles_input1').value;
+//   var bottleinput2 = document.getElementById('bottles_input2').value;
 
-  var answer = (parseInt(bottleinput1) * 0.04);
-  document.getElementById('bottles_results1').value = answer;
+//   var answer = (parseInt(bottleinput1) * 0.04);
+//   document.getElementById('bottles_results1').value = answer;
 
-  var answer2 = (parseInt(bottleinput2) * 0.04);
-  document.getElementById('bottles_results2').value = answer2;
+//   var answer2 = (parseInt(bottleinput2) * 0.04);
+//   document.getElementById('bottles_results2').value = answer2;
 
-  var answer3 = (answer * 2.17);
-  document.getElementById('bottles_results3').value = answer3;
+//   var answer3 = (answer * 2.17);
+//   document.getElementById('bottles_results3').value = answer3;
 
-  var answer4 = (answer2 * 2.17);
-  document.getElementById('bottles_results4').value = answer4;
+//   var answer4 = (answer2 * 2.17);
+//   document.getElementById('bottles_results4').value = answer4;
 
-  var bottleinput3 = document.getElementById('bottles_input').value;
-  var bottleinput4 = document.getElementById('bottles_input0').value;
+//   var bottleinput3 = document.getElementById('bottles_input').value;
+//   var bottleinput4 = document.getElementById('bottles_input0').value;
 
-  //before taking action
-  if (bottleinput3 == "") {
-    answer5 = "";
-    document.getElementById('bottles_results5').value = answer5.toFixed(2);
-  } else if (bottleinput3 == "1.15") {
-    answer5 = answer * 1.15;
-    result = answer5;
-    document.getElementById('bottles_results5').value = answer5.toFixed(2);
-  } else if (bottleinput3 == "0") {
-    answer5 = answer * 0;
-    result = answer5;
-    document.getElementById('bottles_results5').value = answer5.toFixed(2);
-  }
+//   //before taking action
+//   if (bottleinput3 == "") {
+//     answer5 = "";
+//     document.getElementById('bottles_results5').value = answer5.toFixed(2);
+//   } else if (bottleinput3 == "1.15") {
+//     answer5 = answer * 1.15;
+//     result = answer5;
+//     document.getElementById('bottles_results5').value = answer5.toFixed(2);
+//   } else if (bottleinput3 == "0") {
+//     answer5 = answer * 0;
+//     result = answer5;
+//     document.getElementById('bottles_results5').value = answer5.toFixed(2);
+//   }
 
-  //after taking action
-  if (bottleinput4 == "") {
-    answer6 = "";
-    result = answer5;
-    document.getElementById('bottles_results6').value = answer6.toFixed(2);
-  } else if (bottleinput4 == "1.15") {
-    answer6 = answer2 * 1.15;
-    result = answer6;
-    document.getElementById('bottles_results6').value = answer6.toFixed(2);
-  } else if (bottleinput4 == "0") {
-    answer6 = answer2 * 0;
-    result = answer6;
-    document.getElementById('bottles_results6').value = answer6.toFixed(2);
-  }
+//   //after taking action
+//   if (bottleinput4 == "") {
+//     answer6 = "";
+//     result = answer5;
+//     document.getElementById('bottles_results6').value = answer6.toFixed(2);
+//   } else if (bottleinput4 == "1.15") {
+//     answer6 = answer2 * 1.15;
+//     result = answer6;
+//     document.getElementById('bottles_results6').value = answer6.toFixed(2);
+//   } else if (bottleinput4 == "0") {
+//     answer6 = answer2 * 0;
+//     result = answer6;
+//     document.getElementById('bottles_results6').value = answer6.toFixed(2);
+//   }
 
-  console.log(answer5);
-  console.log(answer6);
+//   console.log(answer5);
+//   console.log(answer6);
 
-  var answer7 = (answer3 - answer5);
-  document.getElementById('bottles_results7').value = answer7.toFixed(2);
+//   var answer7 = (answer3 - answer5);
+//   document.getElementById('bottles_results7').value = answer7.toFixed(2);
 
-  var answer8 = (answer7 * 36);
-  document.getElementById('bottles_results8').value = answer8.toFixed(2);
-  bottleCarbonBefore = answer8;
-  document.getElementById("sumPlasticBefore").innerHTML = answer8;
+//   var answer8 = (answer7 * 36);
+//   document.getElementById('bottles_results8').value = answer8.toFixed(2);
+//   bottleCarbonBefore = answer8;
+//   document.getElementById("sumPlasticBefore").innerHTML = answer8;
 
-  var answer9 = (answer4 - answer6);
-  document.getElementById('bottles_results9').value = answer9.toFixed(2);
+//   var answer9 = (answer4 - answer6);
+//   document.getElementById('bottles_results9').value = answer9.toFixed(2);
 
-  var answer10 = (answer9 * 36);
-  document.getElementById('bottles_results10').value = answer10.toFixed(2);
-  bottleCarbonAfter = answer10;
-  document.getElementById("sumPlasticAfter").innerHTML = answer10;
+//   var answer10 = (answer9 * 36);
+//   document.getElementById('bottles_results10').value = answer10.toFixed(2);
+//   bottleCarbonAfter = answer10;
+//   document.getElementById("sumPlasticAfter").innerHTML = answer10;
 
-  var answer11 = (answer7 - answer9);
-  document.getElementById('bottles_results11').value = answer11.toFixed(2);
+//   var answer11 = (answer7 - answer9);
+//   document.getElementById('bottles_results11').value = answer11.toFixed(2);
 
-  var answer12 = (answer11 * 36);
-  document.getElementById('bottles_results12').value = answer12.toFixed(2);
-  document.getElementById("sumPlasticEmissionSavings").innerHTML = answer12;
+//   var answer12 = (answer11 * 36);
+//   document.getElementById('bottles_results12').value = answer12.toFixed(2);
+//   document.getElementById("sumPlasticEmissionSavings").innerHTML = answer12;
 
-}
+// }
 
 // Clear input values for bottles contents
 $('#btn_reset_bottles').click(function () {
